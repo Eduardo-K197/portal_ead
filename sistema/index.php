6e9c2254681d4022be6e6602d2a8d92f2f418d99
Eduardo.K197 <58642173+tisbcpa@users.noreply.github.com>
2022-04-18 10:14:38 -0300

    up 1

--- sistema/index.php
+++ sistema/index.php
@@ -3,11 +3,7 @@ require_once ('conexao.php');
 $senha ='123';
 $senha_crip = md5($senha);
 //criar um usuario administrador caso não exista
-<<<<<<< HEAD
-$pdo->query("insert into usuarios set nome = 'Administrador', cdp = 000.000.000-00, usuario = 'carloseduardobezerradasilva2@gmail.com', senha ='$senha', senha_crip  = 'senha_crip', nivel = 'Administrador', foto = 'sem-perfil,jpg', id_pessoa = 1, ativo = 'Sim', data = curDate());
-=======
 $pdo->query("insert into usuarios set nome = 'Administrador', cpf = 000.000.000-00, usuario = 'carloseduardobezerradasilva2@gmail.com', senha ='$senha', 'senha_crip' = 'senha_crip', 'nivel' = 'Administrador', 'foto' = 'sem-perfil,jpg', 'id_pessoas' = 1, 'ativo' = 'Sim', 'data' = 'curDate()');
->>>>>>> 26bd41913da16b14c117f3d8ba36eac7cd1f4602
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
