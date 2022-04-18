6f960c6866ef15eec073d233d8e922fda057abae
Eduardo.K197 <44842173+Eduardo-K197@users.noreply.github.com>
2022-04-18 00:05:15 -0300

    primeiro commit

---
+++ sistema/autenticar.php
@@ -0,0 +1,2 @@
+<?php
+echo $_post['usuario'];
---
+++ sistema/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Portal de cursos</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<form method="post" action="autenticar.php">
+    <input type="text" name="usuario" placeholder="Email ou CPF">
+    <input type="password" name="senha" placeholder="Digite a sua senha">
+    <button>type="submit">Logar</button>
+</form>
+</body>
+</html>
