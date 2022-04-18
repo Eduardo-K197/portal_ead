23b23f3ca0e55c02f97ce99d091bd13597a7ce43
Eduardo.K197 <44842173+Eduardo-K197@users.noreply.github.com>
2022-04-18 00:08:35 -0300

    primeiro commit

--- sistema/index.php
+++ sistema/index.php
@@ -8,7 +8,7 @@
 <form method="post" action="autenticar.php">
     <input type="text" name="usuario" placeholder="Email ou CPF">
     <input type="password" name="senha" placeholder="Digite a sua senha">
-    <button>type="submit">Logar</button>
+    <button type="submit">Logar</button>
 </form>
 </body>
 </html>
