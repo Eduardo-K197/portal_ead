d0d2aa947e26f566640cec12850e80de1e4109b6
Eduardo.K197 <58642173+tisbcpa@users.noreply.github.com>
2022-04-19 17:28:02 -0300

    painel aluno and admin

--- sistema/autenticar.php
+++ sistema/autenticar.php
@@ -1,4 +1,4 @@
-<?php
+c<?php
 require_once('conexao.php');
 $usuario = $_post['usuario'];
 $senha = $_post['senha'];
@@ -8,7 +8,9 @@ $query = $pdo->query("select * from usuarios where usuario = '$usuario' and
     senha = '$senha'");
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 if (count($res) > 0) {
-    echo 'Usuário existente';
+    //recuperar o nivel do usuario
+    $nivel = $res[0]['nivel'];
+    echo $nive;
 } else {
     echo 'Usuário não existente';
 }
