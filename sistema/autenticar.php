a221d7b3d6f33a636728d8242562ab07d43c80e0
Eduardo.K197 <44842173+Eduardo-K197@users.noreply.github.com>
2022-04-18 21:11:48 -0300

    correção do erro index.php

--- sistema/autenticar.php
+++ sistema/autenticar.php
@@ -1,6 +1,6 @@
 <?php
 require_once('conexao.php');
-$usuario = $_post['usuario'] . '<br>';
+$usuario = $_post['usuario'];
 $senha = $_post['senha'];
 $senha_crip = md5($senha);
 
@@ -8,6 +8,8 @@ $query = $pdo->query("select * from usuarios where usuario = '$usuario' and senh
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 if (count($res) > 0) {
     echo 'Usuário existente';
+}else{
+    echo 'Usuário não existente'
 }
 
 
